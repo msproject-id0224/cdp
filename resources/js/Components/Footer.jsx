a09a2b20c224bb69cc34f657d7f069100b9858cf
msproject-id0224 <mawarsaronp@gmail.com>
2026-03-26 13:56:32 +0900

    Fix copyright notice in Footer component

--- resources/js/Components/Footer.jsx
+++ resources/js/Components/Footer.jsx
@@ -17,7 +17,7 @@ export default function Footer ({ transparent = false }) {
                             : 'text-gray-500 dark:text-gray-400'
                     }`}
                 >
-                    Hak Cipta &copy; {new Date().getFullYear()}
+                    &copy; {new Date().getFullYear()}
                     {' jamiemax & '}
                     {__('CDP Dev/Team')}. {__('All rights reserved.')}
                 </div>
